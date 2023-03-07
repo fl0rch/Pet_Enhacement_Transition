e21a1aab37ed95833617ed256330512bd838f892
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-07 23:04:33 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -316,9 +316,9 @@ def write_restricted_page():
                             'American Bobtail', 'Persian', 'British Shorthair', 'Birman',
                             'Ragdoll', 'Siamese', 'Russian Blue', 'Tuxedo', 'Sphynx' ]
             
-                              prediction = breed_list[pred]
-                                st.write(f'The breed is {prediction}')
-                                #df_adopted = df_adopted.append({"path":x, "name":name_pet, "breed":prediction, "desciption":description_pet}, ignore_index=True)
+                     prediction = breed_list[pred]
+                     st.write(f'The breed is {prediction}')
+                     #df_adopted = df_adopted.append({"path":x, "name":name_pet, "breed":prediction, "desciption":description_pet}, ignore_index=True)
 
                 
     else:
