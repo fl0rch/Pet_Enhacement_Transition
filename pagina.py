e5ecfd9f380f9003e4d7ae74ead64a4dbbc0d570
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-07 22:52:56 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -297,24 +297,25 @@ def write_restricted_page():
                 pred = np.argmax(pet_model.predict(np.expand_dims(img_array, axis=0)))
                 
                  breed_list = [ 'African Wild Dog', 'Basenji', 'American Spaniel', 'Afghan',
-                                'Basset', 'Bearded Collie', 'Beagle', 'Bermaise',
-                                'American Hairless', 'Airedale', 'Bull Terrier', 'Border Collie',
-                                'Borzoi', 'Bloodhound', 'Bluetick', 'Bull Mastiff', 'Blenheim',
-                                'Boxer', 'Boston Terrier', 'Bichon Frise', 'Chinese Crested',
-                                'Chihuahua', 'Cocker', 'Chow', 'Collie', 'Corgi', 'Cockapoo',
-                                 'Clumber', 'Cairn', 'Bulldog', 'German Sheperd',
-                                 'Golden Retriever', 'Great Dane', 'Dhole', 'Coyote',
-                                 'French Bulldog', 'Doberman', 'Elk Hound', 'Dalmation', 'Dingo',
-                                 'Great Perenees', 'Labradoodle', 'Irish Spaniel', 'Greyhound',
-                                  'Lhasa', 'Groenendael', 'Japanese Spaniel', 'Irish Wolfhound',
-                                  'Komondor', 'Labrador', 'Pomeranian', 'Pit Bull', 'Pekinese',
-                                  'Rhodesian', 'Maltese', 'Mex Hairless', 'Malinois', 'Poodle',
-                                  'Pug', 'Newfoundland', 'Shih-Tzu', 'Shiba Inu', 'Rottweiler',
-                                  'Siberian Husky', 'Scotch Terrier', 'Vizsla', 'Saint Bernard',
-                                  'Shar_Pei', 'Schnauzer', 'Yorkie', 'Bombay', 'Bengal',
-                                   'American Shorthair', 'Maine Coon', 'Egyptian Mau', 'Abyssinian',
-                                   'American Bobtail', 'Persian', 'British Shorthair', 'Birman',
-                                   'Ragdoll', 'Siamese', 'Russian Blue', 'Tuxedo', 'Sphynx' ]
+       'Basset', 'Bearded Collie', 'Beagle', 'Bermaise',
+       'American Hairless', 'Airedale', 'Bull Terrier', 'Border Collie',
+       'Borzoi', 'Bloodhound', 'Bluetick', 'Bull Mastiff', 'Blenheim',
+       'Boxer', 'Boston Terrier', 'Bichon Frise', 'Chinese Crested',
+       'Chihuahua', 'Cocker', 'Chow', 'Collie', 'Corgi', 'Cockapoo',
+       'Clumber', 'Cairn', 'Bulldog', 'German Sheperd',
+       'Golden Retriever', 'Great Dane', 'Dhole', 'Coyote',
+       'French Bulldog', 'Doberman', 'Elk Hound', 'Dalmation', 'Dingo',
+       'Great Perenees', 'Labradoodle', 'Irish Spaniel', 'Greyhound',
+       'Lhasa', 'Groenendael', 'Japanese Spaniel', 'Irish Wolfhound',
+       'Komondor', 'Labrador', 'Pomeranian', 'Pit Bull', 'Pekinese',
+       'Rhodesian', 'Maltese', 'Mex Hairless', 'Malinois', 'Poodle',
+       'Pug', 'Newfoundland', 'Shih-Tzu', 'Shiba Inu', 'Rottweiler',
+       'Siberian Husky', 'Scotch Terrier', 'Vizsla', 'Saint Bernard',
+       'Shar_Pei', 'Schnauzer', 'Yorkie',
+        'Bombay', 'Bengal', 'American Shorthair', 'Maine Coon',
+       'Egyptian Mau', 'Abyssinian', 'American Bobtail', 'Persian',
+       'British Shorthair', 'Birman', 'Ragdoll', 'Siamese',
+       'Russian Blue', 'Tuxedo', 'Sphynx']
             
             prediction = breed_list[pred]
             st.write(f'The breed is {prediction}')
