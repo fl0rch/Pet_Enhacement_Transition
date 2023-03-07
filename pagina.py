1a4a26a9ba99e25cb709848de4f1ffe134cdf506
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-07 11:37:19 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -91,6 +91,13 @@ def write_page_2():
     breed_choice = st.selectbox("¿De qué raza te gustaría adoptar?", breeds[animal_choice])
     st.write("Has seleccionado adoptar un", breed_choice, animal_choice)
     st.write("Aquí hay algunas fotos de los", breed_choice,"s disponibles para su adopción:")
+    query = f"{animal_choice} {breed_choice}"
+    response = requests.get(f"https://www.google.com/search?q={query}&tbm=isch")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    image_url = soup.find('img')['src']
+
+    # Mostrar la imagen en la página
+    st.image(image_url, caption=f"{animal_choice} {breed_choice}", use_column_width=True)
 
 def write_page_3():
     st.write("<h2>Donar:</h2>", unsafe_allow_html=True)
