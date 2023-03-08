537da37f2b0ae3448a4160e06325b20f85da01a4
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-08 11:44:18 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -302,9 +302,10 @@ def write_restricted_page():
                animation = json.load(f)
             
             if st.button('Guardar'):
+                animation_id = st_lottie(animation, speed=1, width=200, height=200, key="animation")
                 with st.spinner("Haciendo predicción"):
                     time.sleep(2)  # simulación de la predicción
-                    st_lottie(animation, speed=1, width=200, height=200, key="animation1")
+                    st_lottie(animation, speed=0, width=200, height=200, key="animation1")
                     pet_model = joblib.load('pet_model.pkl')
                     img_array = keras.utils.img_to_array(image)
                     img_array = img_array/255.
