e9536346b540f5ffe32caf9a736753536af6b9ea
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-08 11:42:04 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -302,7 +302,6 @@ def write_restricted_page():
                animation = json.load(f)
             
             if st.button('Guardar'):
-                animation_id = st_lottie(animation, speed=1, width=200, height=200, key="animation")
                 with st.spinner("Haciendo predicción"):
                     time.sleep(2)  # simulación de la predicción
                     st_lottie(animation, speed=0, width=200, height=200, key="animation1")
