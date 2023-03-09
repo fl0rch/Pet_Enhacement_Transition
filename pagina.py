9c8795766467b93c4a9f1f5d414eaaa91127a6c8
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-09 12:48:08 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -27,8 +27,7 @@ from streamlit_lottie import st_lottie
 import time
 import random
 from model import Model
-import SessionState
-from session_state import SessionState
+
 
 
 st.set_page_config(page_title="P.E.T", page_icon=":paw_prints:")
