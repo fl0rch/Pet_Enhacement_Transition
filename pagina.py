0f2a72c2512085d4f2eb68fd609f748ed23bed30
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-14 22:52:48 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -115,18 +115,15 @@ def write_page_2():
     
 
     for index, row in df_adopted.iterrows():
-     if row['path'] is not None and os.path.exists(IMG_DIR) and row['breed'] == breed_choice:
-        img_path = f"https://github.com/fl0rch/Pet_Enhacement_Transition/tree/main/img_predict/{row['path']}"
-
-        print(img_path)
-        # Mostrar la imagen
-        img = Image.open(img_path)
-        st.image(img, caption=f"{breed_choice} imagen", width=300)
-        st.write("**Nombre:**", row['name'])
-        st.write("**Descripción:**", row['description'])
-     else:
-        # Mostrar mensaje de que la imagen no está disponible
-        st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
+        if row['path'] is not None and os.path.exists(IMG_DIR) and row['breed'] == breed_choice:
+            # Mostrar la imagen
+            img = Image.open(f"{IMG_DIR}/{row['path']}")
+            st.image(img, caption=f"{breed_choice} imagen", width=300)
+            st.write("**Nombre:**", row['name'])
+            st.write("**Descripción:**", row['description'])
+        else:
+            # Mostrar mensaje de que la imagen no está disponible
+            st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
 
     
 def write_page_3():
