4f400f7ca3770b24a3d2cba1c45961fb2122d6ae
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-15 00:57:42 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -126,13 +126,13 @@ def write_page_2():
     img_dir = os.path.join(repo_dir, "img_predict")
 
     for index, row in df_adopted.iterrows():
-    if row['path'] is not None and os.path.exists(img_dir) and row['breed'] == breed_choice:
+     if row['path'] is not None and os.path.exists(img_dir) and row['breed'] == breed_choice:
         # Mostrar la imagen
         img = Image.open(f"{img_dir}/{row['path']}")
         st.image(img, caption=f"{breed_choice} imagen", width=300)
         st.write("**Nombre:**", row['name'])
         st.write("**Descripción:**", row['description'])
-    else:
+     else:
         # Mostrar mensaje de que la imagen no está disponible
         st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
 
