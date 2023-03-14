93eccd0f05f44e033289ff3d010a48b627e4897c
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-14 23:50:39 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -116,17 +116,15 @@ def write_page_2():
     
 
     for index, row in df_adopted.iterrows():
-     img_path = f"{IMG_DIR}/{row['path']}"
-     print(img_path)  # imprime la ruta completa de la imagen
-     if row['path'] is not None and os.path.exists(img_path) and row['breed'] == breed_choice:
-        # Mostrar la imagen
-        img = Image.open(img_path)
-        st.image(img, caption=f"{breed_choice} imagen", width=300)
-        st.write("**Nombre:**", row['name'])
-        st.write("**Descripción:**", row['description'])
-     else:
-        # Mostrar mensaje de que la imagen no está disponible
-        st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
+        if row['path'] is not None and os.path.exists(IMG_DIR) and row['breed'] == breed_choice:
+            # Mostrar la imagen
+            img = Image.open(f"{IMG_DIR}/{row['path']}")
+            st.image(img, caption=f"{breed_choice} imagen", width=300)
+            st.write("**Nombre:**", row['name'])
+            st.write("**Descripción:**", row['description'])
+        else:
+            # Mostrar mensaje de que la imagen no está disponible
+            st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
 
 
 
