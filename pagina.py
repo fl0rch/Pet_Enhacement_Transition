bb87bd8264e3e61325d1cce83509eac718a92273
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-15 01:48:21 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -136,7 +136,7 @@ def write_page_2():
         
         
         st.write("Otras imágenes disponibles:")
-         for img_name in os.listdir(img_dir):
+        for img_name in os.listdir(img_dir):
             img_path = os.path.join(img_dir, img_name)
             img = Image.open(img_path)
             st.image(img, caption=img_name, width=300
