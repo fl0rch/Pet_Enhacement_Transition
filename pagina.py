90789a36adb21afb783bbf912699136597fe2f78
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2023-03-15 01:06:00 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -125,16 +125,19 @@ def write_page_2():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     img_dir = os.path.join(repo_dir, "img_predict")
 
-    for index, row in df_adopted.iterrows():
-     if row['path'] is not None and os.path.exists(img_dir) and row['breed'] == breed_choice:
-        # Mostrar la imagen
-        img = Image.open(f"{img_dir}/{row['path']}")
-        st.image(img, caption=f"{breed_choice} imagen", width=300)
-        st.write("**Nombre:**", row['name'])
-        st.write("**Descripción:**", row['description'])
-     else:
-        # Mostrar mensaje de que la imagen no está disponible
-        st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
+   for index, row in df_adopted.iterrows():
+    if row['path'] is not None and row['breed'] == breed_choice:
+        # Cargar la imagen correspondiente
+        img_path = os.path.join(repo_dir, "img_predict", row['path'])
+        if os.path.exists(img_path):
+            img = Image.open(img_path)
+            st.image(img, caption=f"{breed_choice} imagen", width=300)
+            st.write("**Nombre:**", row['name'])
+            st.write("**Descripción:**", row['description'])
+        else:
+            st.warning(f"La imagen de la raza '{breed_choice}' no está disponible.")
+    else:
+        st.warning(f"No hay información disponible para la raza '{breed_choice}'.")
 
         # Mostrar otras imágenes disponibles
         st.write('Otras imagenes disponibles')
